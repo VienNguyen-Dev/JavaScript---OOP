bdc499e0a632b801b8754713f8a6d2e7c1666f78
VienNguyen-Dev <149781283+VienNguyen-Dev@users.noreply.github.com>
2024-05-02 09:01:54 +0700

    convert product into class

--- data/products.js
+++ data/products.js
@@ -1,3 +1,5 @@
+import formatCurrency from "../scripts/utils/money";
+
 export function getProduct(productId) {
   let matchingProduct;
 
@@ -9,6 +11,48 @@ export function getProduct(productId) {
 
   return matchingProduct;
 }
+
+class Product {
+  id;
+  image;
+  name;
+  rating;
+  priceCents;
+  keywords;
+
+  constructor(productDetail) {
+    this.id = productDetail.id;
+    this.image = productDetail.image;
+    this.name = productDetail.name,
+      this.rating = productDetail.rating;
+    this.priceCents = productDetail.priceCents;
+    this.keywords = productDetail.keywords
+  };
+
+
+  getStarUrl() {
+    return `images/ratings/rating-${this.rating.stars * 10}.png`
+  };
+  getPrice() {
+    return `$${formatCurrency(this.priceCents)}`
+  }
+}
+
+const product1 = new Product({
+  id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+  image: "images/products/athletic-cotton-socks-6-pairs.jpg",
+  name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
+  rating: {
+    stars: 4.5,
+    count: 87
+  },
+  priceCents: 1090,
+  keywords: [
+    "socks",
+    "sports",
+    "apparel"
+  ]
+})
 
 export const products = [
   {
@@ -669,4 +713,4 @@ export const products = [
       "mens"
     ]
   }
-];
+].map((product) => new Product(product));
--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -1,10 +1,10 @@
-import {cart, removeFromCart, updateDeliveryOption} from '../../data/cart.js';
-import {products, getProduct} from '../../data/products.js';
-import {formatCurrency} from '../utils/money.js';
-import {hello} from 'https://unpkg.com/supersimpledev@1.0.1/hello.esm.js';
+import { cart, removeFromCart, updateDeliveryOption } from '../../data/cart.js';
+import { products, getProduct } from '../../data/products.js';
+import { formatCurrency } from '../utils/money.js';
+import { hello } from 'https://unpkg.com/supersimpledev@1.0.1/hello.esm.js';
 import dayjs from 'https://unpkg.com/dayjs@1.11.10/esm/index.js';
-import {deliveryOptions, getDeliveryOption} from '../../data/deliveryOptions.js';
-import {renderPaymentSummary} from './paymentSummary.js';
+import { deliveryOptions, getDeliveryOption } from '../../data/deliveryOptions.js';
+import { renderPaymentSummary } from './paymentSummary.js';
 
 export function renderOrderSummary() {
   let cartSummaryHTML = '';
@@ -44,7 +44,7 @@ export function renderOrderSummary() {
               ${matchingProduct.name}
             </div>
             <div class="product-price">
-              $${formatCurrency(matchingProduct.priceCents)}
+             ${matchingProduct.getPrice()}
             </div>
             <div class="product-quantity
               js-product-quantity-${matchingProduct.id}">
@@ -136,7 +136,7 @@ export function renderOrderSummary() {
   document.querySelectorAll('.js-delivery-option')
     .forEach((element) => {
       element.addEventListener('click', () => {
-        const {productId, deliveryOptionId} = element.dataset;
+        const { productId, deliveryOptionId } = element.dataset;
         updateDeliveryOption(productId, deliveryOptionId);
         renderOrderSummary();
         renderPaymentSummary();
